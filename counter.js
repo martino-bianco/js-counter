ec13e961cc305248e7049c84a9614c9ddf8ebe54
Martino-Bianco <martin.g.doerr@gmail.com>
2023-10-01 12:06:50 +0200

    working counter

--- counter.js
+++ counter.js
@@ -9,6 +9,26 @@ const buttons = document.querySelectorAll(".btn");
 
 console.log(buttons);
 
-// buttons.forEach(function (item) {
-//   console.log(item);
-// });
+buttons.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "black";
+    }
+    value.textContent = count;
+    console.log(e.currentTarget.classList);
+  });
+});
