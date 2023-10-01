63d9499a2c53b0cc60f5a54c7cc4de348ce24bb7
Martino-Bianco <martin.g.doerr@gmail.com>
2023-10-01 11:50:08 +0200

    adding logic

---
+++ counter.js
@@ -0,0 +1,14 @@
+// initial value
+
+let count = 0;
+
+// select value and buttons
+
+const value = document.querySelector("#value");
+const buttons = document.querySelectorAll(".btn");
+
+console.log(buttons);
+
+// buttons.forEach(function (item) {
+//   console.log(item);
+// });
